e926ee3a13feb4b39344ad94e08eaae04d2b851a
Victor Dekker <Victor@Ice.site>
2013-05-20 16:01:07 +0200

    Testing adminStory

--- src/config.php
+++ src/config.php
@@ -315,9 +315,8 @@ function adminStory($bericht,$adres) {
 		$verhaal = sqlEscape($onderdelen[4]);
 		$geslacht = ($onderdelen[5] == "NULL") ? 
 			"NULL" : "'" . sqlEscape($onderdelen[5]) . "'";
-		echo "Rol: $rol, fase: $fase, levend: $levend en dood: $dood.\n";
-		echo "Verhaal: $verhaal.\n";
 		if(empty($rol) || !is_int($fase) || empty($verhaal)) {
+			echo "Fout: rol, fase of verhaal ontbreekt.\n";
 			continue;
 		}
 		if(is_int($levend)) {
@@ -326,6 +325,7 @@ function adminStory($bericht,$adres) {
 		if(is_int($dood)) {
 			$dood = "NULL";
 		}
+		echo "Rol: $rol, fase: $fase, levend: $levend en dood: $dood.\n";
 		$sql = "INSERT INTO Verhalen(THEMA,AUTEUR,LEVEND,DOOD,ROL,FASE,";
 		$sql .= "VERHAAL,GESLACHT) VALUES ('$thema','$auteur',$levend,$dood,";
 		$sql .= "'$rol',$fase,'$verhaal',$geslacht)";
